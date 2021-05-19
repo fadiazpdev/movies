c9f1fde96afcca640dd2231c50fa8c7b73286039
Andres Diaz <fadiazp.dev@gmail.com>
2021-05-19 03:41:33 -0500

    * Citing borrowed code

--- app/src/main/java/com/fadiazp/movies/presentation/common/Extensions.kt
+++ app/src/main/java/com/fadiazp/movies/presentation/common/Extensions.kt
@@ -1,11 +1,18 @@
 package com.fadiazp.movies.presentation.common
 
-import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
 import com.fadiazp.movies.App
+
+/***************************************************************************************
+ *    Title: Hacking Architecture Components by using Kotlin
+ *    Author: Antonio Leiva
+ *    Date: March 2018
+ *    Availability: https://antonioleiva.com/architecture-components-kotlin/
+ *
+ ***************************************************************************************/
 
 @Suppress("UNCHECKED_CAST")
 inline fun <reified T : ViewModel> Fragment.getViewModel(crossinline factory: () -> T): T {
